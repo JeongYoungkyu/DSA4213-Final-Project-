96ae161ff357c81e0198d72f126e8ef63d524b72
JeongYoungkyu <e0957742@u.nus.edu>
2025-11-16 22:21:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import json
+import torch
+from datasets import Dataset, load_dataset
+from transformers import (
+    GPT2TokenizerFast,
+    GPT2LMHeadModel,
+    TrainingArguments,
+    Trainer,
+    DefaultDataCollator,
+)
+from peft import LoraConfig, get_peft_model, PeftModel
+
 from .data import main as data_main
 from .gpt_2_small import main as gpt_2_small_main
 from .gpt_2_large import main as gpt_large_main
